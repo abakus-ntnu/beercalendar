c222f844885da03f0fdd32804f58bd43e63e9d26
Ivar Nakken <ivarnakken1441@gmail.com>
2021-12-01 14:45:34 +0100

    Add webpack config

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -1,7 +1,12 @@
 import styles from "./Header.module.css";
 
 const SubHeader = () => {
-  let options = { weekday: 'long', year: 'numeric', month: 'long', day: '2-digit' };
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
   // @ts-ignore :)
   const today = new Date().toLocaleDateString("no", options);
   return (
@@ -9,7 +14,13 @@ const SubHeader = () => {
       <div className={styles.subChristmas}>
         {today}
         <div className={styles.trailer}>
-          <iframe src="https://www.youtube.com/embed/odFnKFkRI3k" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe
+            src="https://www.youtube.com/embed/odFnKFkRI3k"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </>
